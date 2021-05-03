8264a986a2165520bc50c426fc78536e3fa34842
Klemensas Dranseika <hello@klemensas.net>
2021-05-03 19:45:53 +0300

    Updates

---
+++ precache-manifest.f61db2afb377451c959ddf8842ac284b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d8af8129418ccca5ca0f29f18655ff",
+    "url": "/catan-stats/index.html"
+  },
+  {
+    "revision": "8369a7e4c6d4065b3430",
+    "url": "/catan-stats/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4fb5e1371d56c0344271",
+    "url": "/catan-stats/static/js/2.34528251.chunk.js"
+  },
+  {
+    "revision": "c1ca0d82fd72f465c96372887106b05d",
+    "url": "/catan-stats/static/js/2.34528251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8369a7e4c6d4065b3430",
+    "url": "/catan-stats/static/js/main.80f9ce6c.chunk.js"
+  },
+  {
+    "revision": "2bac4f45a5ff37d1e808",
+    "url": "/catan-stats/static/js/runtime-main.4e7fa856.js"
+  }
+]);
